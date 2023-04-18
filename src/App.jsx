806708a85b580ba1a6e4c20152b5e48e0f269f0b
zzeongho <junghoo022@gmail.com>
2023-04-18 16:28:39 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,72 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  const [content, setContent] = useState("");
+  const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmitChat = async (e) => {
+    try {
+      e.preventDefault();
+
+      if (!content) return;
+
+      setIsLoading(true);
+
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/chat`,
+        {
+          content,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.REACT_APP_SECRET_KEY}`,
+          },
+        }
+      );
+
+      setResult(response.data.result);
+
+      setIsLoading(false);
+      setContent("");
+    } catch (error) {
+      console.error(error);
+
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_SECRET_KEY);
+  }, []);
+
+  return (
+    <div className="max-w-screen-md mx-auto min-h-screen flex flex-col justify-start items-center pt-16 px-4">
+      <form className="flex w-full" onSubmit={onSubmitChat}>
+        <input
+          className={`grow border-2 px-2 py-1 border-gray-300 rounded-lg focus:outline-main shadow-lg ${
+            isLoading && "bg-gray-200 text-gray-400"
+          }`}
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          disabled={isLoading}
+        />
+        <input
+          className={`w-24 ml-4 px-2 py-1 border-2 border-main text-main rounded-lg shadow-lg ${
+            isLoading && "bg-main text-white"
+          }`}
+          type="submit"
+          disabled={isLoading}
+          value={isLoading ? "검색중..." : "검색"}
+        />
+      </form>
+      {result && <div className="mt-16 bg-main p-4 text-gray-50">{result}</div>}
+    </div>
+  );
 }
 
 export default App;
